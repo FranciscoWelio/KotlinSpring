adfdf1964fb94ca9e50a7164cfd6cc27fd3bc2ce
Francisco Welio <fwelio.dev@gmail.com>
2025-02-14 22:29:43 -0300

    first commit

---
+++ demo-teste/src/main/kotlin/br/com/teste/demo_teste/DemoTesteApplication.kt
@@ -0,0 +1,11 @@
+package br.com.teste.demo_teste
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoTesteApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoTesteApplication>(*args)
+}
---
+++ demo-teste/src/test/kotlin/br/com/teste/demo_teste/DemoTesteApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.teste.demo_teste
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoTesteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
