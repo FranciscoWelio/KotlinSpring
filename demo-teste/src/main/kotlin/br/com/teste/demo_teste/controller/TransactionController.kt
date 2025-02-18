80ce3fb887f2185215b680e5c26389e6f3fb280c
Francisco Welio <fwelio.dev@gmail.com>
2025-02-18 17:58:40 -0300

    feat/ajustes Transaction Controller e service

--- demo-teste/src/main/kotlin/br/com/teste/demo_teste/controller/TransactionController.kt
+++ demo-teste/src/main/kotlin/br/com/teste/demo_teste/controller/TransactionController.kt
@@ -3,6 +3,11 @@ package br.com.teste.demo_teste.controller
 import br.com.teste.demo_teste.entity.Transaction
 import br.com.teste.demo_teste.request.TransactionReq
 import br.com.teste.demo_teste.service.TransactionService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,8 +16,11 @@ class TransactionController(private var transactionService: TransactionService) 
 
 
     @PostMapping("fazendo-transaction")
+    @Operation(summary = "Cria Transação", description = "Cria uma transação com o Id-Usuario, Id-Loja e valor-de-compra")
     fun fazerTransaction(@RequestBody  requisition: TransactionReq):Transaction{
-        return transactionService.fazerTransaction(requisition.usuarioId, requisition.merchantId, requisition.amount)
+         val transction = transactionService.fazerTransaction(requisition.usuarioId, requisition.merchantId, requisition.amount)
+
+        return transction
     }
 
     @GetMapping("/{id}")
--- demo-teste/src/main/kotlin/br/com/teste/demo_teste/service/TransactionService.kt
+++ demo-teste/src/main/kotlin/br/com/teste/demo_teste/service/TransactionService.kt
@@ -8,51 +8,63 @@ import br.com.teste.demo_teste.exception.UsuarioException
 import br.com.teste.demo_teste.repository.MerchantRepository
 import br.com.teste.demo_teste.repository.TransactionRepository
 import br.com.teste.demo_teste.repository.UsuarioRepository
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.transaction.Transactional
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.util.*
 
+@Suppress("UNREACHABLE_CODE")
 @Service
 class TransactionService(private var transactionRepository :TransactionRepository,
                          private var usuarioRepository: UsuarioRepository,
                          private var merchantRepository: MerchantRepository) {
 
     @Transactional
-    fun fazerTransaction(usuarioId: Long, merchantId:Long, amount:Double ): Transaction{
+    fun fazerTransaction(usuarioId: Long, merchantId:Long, amount:Double ):Transaction {
         val usuarioOP: Optional<Usuario> = usuarioRepository.findById(usuarioId)
         val usuario = usuarioOP.get()
         val merchantOP: Optional<Merchant> = merchantRepository.findById(merchantId)
         val merchant = merchantOP.get()
-
         if (merchant.mcc == "5411" || merchant.mcc =="5412"){
-            if (usuario.food<amount){
-                throw UsuarioException("Saldo Alimentação insuficiente")
-            }
-            usuario.food -= amount
-            usuarioRepository.save(usuario)
-        }else if (merchant.mcc == "5811" || merchant.mcc =="5812"){
-            if (usuario.meal<amount){
-                throw UsuarioException("Saldo Refeição insuficiente")
-            }
-            usuario.meal -= amount
-            usuarioRepository.save(usuario)
+            if (usuario.food<amount &&  usuario.cash>=amount){
+                usuario.cash -= amount
+                usuarioRepository.save(usuario)
 
-        }else {
+            }else if(usuario.food<amount &&  usuario.cash<amount){
+                throw UsuarioException("Saldo insuficiente")
+            }else{
+                usuario.food -= amount
+                usuarioRepository.save(usuario)
+            }
+
+        }else if (merchant.mcc == "5811" || merchant.mcc =="5812"){
+            if (usuario.meal<amount &&  usuario.cash>=amount){
+                usuario.cash -= amount
+                usuarioRepository.save(usuario)
+            }else if(usuario.meal<amount &&  usuario.cash<amount){
+                throw UsuarioException("Saldo insuficiente")
+
+            }else{
+                usuario.meal -= amount
+                usuarioRepository.save(usuario)
+            }
+        }else if(merchant.mcc != "5811" && merchant.mcc != "5812" && merchant.mcc != "5411" && merchant.mcc != "5412"){
             if (usuario.cash<amount){
                 throw UsuarioException("Saldo insuficiente")
             }
             usuario.cash -= amount
             usuarioRepository.save(usuario)
         }
-
-        val merchantLocal = merchant.nome +" "+ merchant.localizacao
-        var transaction :Transaction
-
-        transaction = Transaction()
-        transaction.amount = amount
-        transaction.account = usuario.conta
-        transaction.mcc = merchant.mcc
-        transaction.merchant = merchantLocal
+            val merchantLocal = merchant.nome +" "+ merchant.localizacao
+            var transaction :Transaction = Transaction()
+            transaction.amount = amount
+            transaction.account = usuario.conta
+            transaction.mcc = merchant.mcc
+            transaction.merchant = merchantLocal
         return transactionRepository.save(transaction)
     }
 
